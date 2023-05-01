1f26c5cd838d5f2788e5d85ec6bb1941447cb3f3
George Curtis <66566475+grcurtis27@users.noreply.github.com>
2023-05-01 14:54:17 -0400

    Update sample.py
    Slight revision of the original

--- sample.py
+++ sample.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python
+import os
+
+
+def clear():
+    """Clear the terminal screen (x-platform: Linux, Mac, Windows)."""
+    os.system('clear') if os.name == 'posix' else os.system('cls')
+# end of clear() definition.
+
 
 def main():
+    clear()
     print("Hello, World!")
     return 0
 # end of main() definition.
 
 
-main()
-
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\n\nprocessing aborted by user. exiting...')
+    finally:
+        exit('\nThanks for flying GeoCur!')
